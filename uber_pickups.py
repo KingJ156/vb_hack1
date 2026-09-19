0f303e8899cab5a8f82652785462c280fc558090
KingJ156 <jasonyoo1123@gmail.com>
2026-09-19 19:44:27 +0000

    c

--- uber_pickups.py
+++ uber_pickups.py
@@ -9,3 +9,6 @@ if st.button('Say hello'):
      st.write('Why hello there')
 else:
      st.write('Goodbye')
+
+number = st.number_input("Insert a number")
+st.write("The current number is ", number)
